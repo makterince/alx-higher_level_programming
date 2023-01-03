7587f90931ff9397d09dff6dec3515e8c6bab2aa
Festus Omaku <fomaku3@gmail.com>
2023-01-03 23:26:21 +0100

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for small_alpha in range(ord('a'), ord('z')+1):
-    small_alphabet = chr(small_alpha)
-    if small_alphabet not in "qe":
-        print(small_alphabet, end="")
+for i in range(97,123):
+        print("{:c}".format(i), end="")
