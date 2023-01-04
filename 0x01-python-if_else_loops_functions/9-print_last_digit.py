d832179a35950a49330c4fdb53cfaa6bd78fb7ff
Festus Omaku <fomaku3@gmail.com>
2023-01-04 19:50:43 +0100

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    #if the number is negative
     if number < 0:
         last_digit = number % -(10)
         print(-(last_digit), end="")
+        #if the number is positive
     else:
         last_digit = number % 10
         print(last_digit, end="")
