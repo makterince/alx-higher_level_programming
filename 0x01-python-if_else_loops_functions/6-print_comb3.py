eb722e607e39e386a75fa3903b6d3fc7889d3fa2
Festus Omaku <fomaku3@gmail.com>
2023-01-04 19:11:33 +0100

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-combo = []
-for i in range(10):
-    for j in range(10):
-        if (i != j and i < j):
-            print(f"{i}{j}", end='\n' if combo == 89 else ", ")
+combo = 0
+while combo <= 89:
+    if combo % 10 == 0:
+        combo += 1 + combo // 10
+    print("{:02d}".format(combo), end="\n" if combo == 89 else ", ")
+    combo += 1
