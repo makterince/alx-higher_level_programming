9d7ae8b4ea29d7285e4e5fdd72471df0171817ae
Festus Omaku <fomaku3@gmail.com>
2023-01-04 20:08:48 +0100

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100):
+        if i % 15 == 0:
+            print("FizzBuzz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 == 0:
+            print("Fizz", end="")
+    print("{} ".format(number), edn="")
