17964f229493be55f65b7780548de483b22df46a
Festus Omaku <fomaku3@gmail.com>
2023-01-04 01:30:10 +0100

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,4 @@ combo = []
 for i in range(10):
     for j in range(10):
         if (i != j and i < j):
-            combo.append(f"{i}{j}")
-
-print(", ".join(combo))
+            print(f"{i}{j}", end='\n' if combo == 89 else ", ")
