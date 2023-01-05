4f5a5d835907a7ff68218d8185d450a02f3fd613
Festus Omaku <fomaku3@gmail.com>
2023-01-05 16:12:46 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-n = len(sys.argv)
-for i in range(1, n):
-    ("{}: ".format(sys.argv), sys.argv[i], end='\n')
+if __name__ == "__main__":
+    from sys import argv
+    n = len(sys.argv)
+    
+    for i in range(1, n):
+        ("{}: ".format(sys.argv), sys.argv[i], end='\n')
