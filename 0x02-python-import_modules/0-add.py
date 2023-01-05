c828b104d927fe245cf010627b278cdc8c77c962
Festus Omaku <fomaku3@gmail.com>
2023-01-05 15:09:14 +0100

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+print("add_0", sys.argv[0])
+n = len(sys.argv)
+
+
+def add(a, b):
+    a = 1
+    b = 2
+
+
+sum = 0
+for i in range(1, n):
+    sum += int(sys.argv[i])
+print("{} + {} = {}".format(a, b, sum), end='')
