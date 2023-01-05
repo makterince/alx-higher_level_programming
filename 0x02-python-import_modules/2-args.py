7d7d7c24379beac3024e0ee98ae5d25af0c2a816
Festus Omaku <fomaku3@gmail.com>
2023-01-05 16:18:49 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,17 @@
 if __name__ == "__main__":
     from sys import argv
     n = len(sys.argv)
-    
-    for i in range(1, n):
-        ("{}: ".format(sys.argv), sys.argv[i], end='\n')
+    i = 0
+
+    if n == 2:
+        print("{}argument:".format(n - 1))
+    elif n == 1:
+        print("{}argument:".format(n - 1))
+    else:
+        print{"{}argument:".format(n - 1))
+    for i in argv:
+        if arg == 0:
+            i += 1
+            continue
+        print("{}: {}".format(i, arg))
+        i += 1
