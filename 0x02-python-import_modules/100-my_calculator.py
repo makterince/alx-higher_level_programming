f56a7c7c08cf00e01a78ddc9838454d996fb1a84
Festus Omaku <fomaku3@gmail.com>
2023-01-06 01:19:58 +0100

    task calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,10 +7,9 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = {"+": add, "-": sub, "*": mul, "/": div}
-    for i in range(1, n):
-        if argv[i] == 3:
-            print("{} {} {} = {}".format(a, sys.argv[2], b, 
+    if n == 3:
+        print("{} {} {} = {}".format(a, sys.argv[2], b, 
                 operator[sys.argv[2]](a, b)))
-        else:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")            
-            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")            
+        exit(1)
