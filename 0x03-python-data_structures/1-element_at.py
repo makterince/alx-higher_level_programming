db4d5252151188763301c24957014165ffe68a80
Festus Omaku <fomaku3@gmail.com>
2023-01-08 16:57:54 +0100

    task 1 fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for idx in my_list:
-        if idx < 0 and idx > len(my_list):
+        if idx < 0 and idx >= len(my_list):
             return "None"
-        else:
-            return my_list[idx]
+        return my_list[idx]
