c7ee79138f562b7c03b74041870429c165618169
Festus Omaku <fomaku3@gmail.com>
2023-01-08 18:21:45 +0100

    task 1 fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    print("{::-1d}".format(my_list[]), end='\n')
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(my_list))
+        else:
+            return None
