cf4471e9d00983a5ac42cacf41a2aa3c7621338c
Festus Omaku <fomaku3@gmail.com>
2023-01-08 23:32:28 +0100

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
    new_string = []
    for i in range(len(my_string)):
-       if my_string[i] != 'c' or my_string[i] != 'C':
+       if i != 'c' and i != 'C':
            new_string.append(my_string[i])
         return new_string
