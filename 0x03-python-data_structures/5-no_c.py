be0412c48cf3b0a9f4e9f0d241a4afc1bd26ed44
Festus Omaku <fomaku3@gmail.com>
2023-01-08 22:24:56 +0100

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-   i = 0
-   while (i < len(my_string)):
-       if (my_string[i] == 'c' or my_string[i] == 'C'):
-           new_string = my_string[:i] + my_string[i+1:]
-           i += 1
-        i += 1
-    return new_string
+   new_string = ''
+   for char in my_string:
+       if char != 'c' and char != 'C':
+           new_string += char
+        return new_string
