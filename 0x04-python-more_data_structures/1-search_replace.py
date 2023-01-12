ab192a16db966f0d82b9e98406a69e47a58a4b67
Festus Omaku <fomaku3@gmail.com>
2023-01-12 01:06:21 +0100

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for i in range(len(my_list)):
+    for i in len(my_li):
         if my_list[i] == search:
             my_list[i} = replace
     return new_list
