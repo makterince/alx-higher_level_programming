bd5536a02506880d580d95b946e3f02616bad294
Festus Omaku <fomaku3@gmail.com>
2023-01-23 17:17:10 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    value = 0
     for i in range(x):
         try:
-            x = my_list[i]
-            print(x, end="")
-            value += 1
+            print(my_list[i], end="")
+            i += 1
         except ValueError:
             pass
     print()
-    return(value)
+    return(x)
