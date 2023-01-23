7505138aca715c7e3e45b7d8a7798836031f841f
Festus Omaku <fomaku3@gmail.com>
2023-01-23 17:12:45 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-try:
-    print(x)
-except ValueError:
-    print("an error occured")
-except:
-    print("x is too big")
+    for i in range(x):
+        try:
+            x = my_list[i]
+            print(x, end="")
+            i += 1
+        except ValueError:
+            pass
+    print()
+    return(x)
