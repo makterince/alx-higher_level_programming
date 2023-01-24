d2f7ec9ba01eda63fe1719713d29c77c48f8aba0
Festus Omaku <fomaku3@gmail.com>
2023-01-24 01:06:20 +0100

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             value += 1
         except (TypeError, ValueError):
             pass
-        print()
-        return (value)
+    print()
+    return (value)
