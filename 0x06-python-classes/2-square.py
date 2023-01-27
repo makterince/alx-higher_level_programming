038732ea57bc12bcc6ba4b927c744c8203a43990
Festus Omaku <fomaku3@gmail.com>
2023-01-28 00:00:27 +0100

    task 3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """ creating an instance of it with size """
 
-    def __init__(self, size=None):
+    def __init__(self, size=0):
         """ raising an exception """
 
         if not isinstance(size, int):
