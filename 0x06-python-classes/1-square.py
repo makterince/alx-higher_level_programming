1fa2b61b092f21fb35ae6c3c705b0f89357e1914
Festus Omaku <fomaku3@gmail.com>
2023-01-27 23:23:02 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" creating a square class """
+
+
+class Square:
+    """ creating an instance of it with size """
+    def __init__(self, size=None):
+        self.__size = size
