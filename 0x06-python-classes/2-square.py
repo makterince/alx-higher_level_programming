5b6f3ad53ed3c1d9e9fc1af3141298b3f4a739e9
Festus Omaku <fomaku3@gmail.com>
2023-01-27 23:43:08 +0100

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,9 @@
 class Square:
     """ creating an instance of it with size """
     def __init__(self, size=None):
-        self.__size = size
         """ raising an exception """
         if type(size) != int():
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
