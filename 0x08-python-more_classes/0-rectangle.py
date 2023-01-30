4658f4b658156ea095a261475cb9d6664facc63d
Festus Omaku <fomaku3@gmail.com>
2023-01-31 00:09:00 +0100

    task 0 -1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 """ Create an empty class Rectangle """
+
+
 class Rectangle:
+    """ empty class """
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
 """ Create an empty class Rectangle """
+
+
 class Rectangle:
+    """ rectangle, its width and height"""
+
     def __init__(self, width=0, height=0)
-    self.__width = width
-    self.__height = height
+    """ new instance """
+
+    self.width = width
+    self.height = height
 
     @property
     def width(self, width):
+        """ width property """
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ set width """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
     @property
     def height(self):
+        """ get height """
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ set height """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
