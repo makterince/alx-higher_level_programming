c0c99d6a1269f0cfed7ae77a8a950fedf79204ce
Festus Omaku <fomaku3@gmail.com>
2023-01-31 00:54:54 +0100

    task 4

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Rectangle class the defines height, width and perimeter"""
+
+
+class Rectangle:
+    """ rectangle, its width and height"""
+    def __init__(self, width=0, height=0):
+        """ new instance """
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """ width property """
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """ set width """
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """ get height """
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """ set height """
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+
+        def area(self):
+            """ area of the rectangle """
+            return self.__width * self.__height
+
+        def perimeter(self):
+            """ perimeter of a rectangle """
+            if self.__width == 0 or self.__height == 0:
+                return 0
+            return 2 * (self.__width + self.__height)
+
+        def __str__(self):
+            """ print rectangle with #"""
+            if self.__width == 0 or self.height == 0:
+                return ""
+            else:
+                return "\n".join(["#"*self.__width for i in
+                                 range(self.__height)])
