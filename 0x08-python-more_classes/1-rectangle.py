5061155424a585e7fa41bea7ea0c0b0922dce85d
Festus Omaku <fomaku3@gmail.com>
2023-01-31 00:10:49 +0100

    task 0 -1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """ rectangle, its width and height"""
 
-    def __init__(self, width=0, height=0)
+    def __init__(self, width=0, height=0):
     """ new instance """
 
     self.width = width
