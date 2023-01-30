1ef5311214da38655a4dd1d74515b807bbfceab3
Festus Omaku <fomaku3@gmail.com>
2023-01-31 00:28:12 +0100

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
     def width(self):
         """ width property """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """ set width """
@@ -23,6 +23,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """ get height """
