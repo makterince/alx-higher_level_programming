15c674d9b7e074156a412f1977dc3f8add8db0b5
Festus Omaku <fomaku3@gmail.com>
2023-02-07 11:52:37 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" creating an empty class """
+
+
+class BaseGeometry:
+    """ not empty anymore """
+
+    def area(self):
+        """ raise an exception """
+
+        raise Exception("area() is not implemented")
+
