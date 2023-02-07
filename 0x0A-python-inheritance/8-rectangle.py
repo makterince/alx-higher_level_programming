e3a47d9f8381982b51a09cfcd166ae9be0635bd7
Festus Omaku <fomaku3@gmail.com>
2023-02-07 18:40:38 +0100

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ creating a class that inherits from basegeometry """
 
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """ initialize the parameters """
