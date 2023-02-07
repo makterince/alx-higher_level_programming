80c1fc5ef209fe7c98fc38cabc618c00ff15c596
Festus Omaku <fomaku3@gmail.com>
2023-02-07 17:38:12 +0100

    task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
     def area(self):
         """ raise an exception """
 
-    raise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """ checking if it is an instance"""
