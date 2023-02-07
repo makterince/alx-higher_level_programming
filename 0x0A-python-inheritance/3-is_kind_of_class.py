a28878b072648e939fcdcd506329a5ad840c220e
Festus Omaku <fomaku3@gmail.com>
2023-02-07 11:29:23 +0100

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" checking if obj is an instance of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """utilising the builtin isinstance of"""
+
+    return isinstance(obj, a_class)
