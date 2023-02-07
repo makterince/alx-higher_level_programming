8cb314ec3bfab5fa28608e247efe4303452e2534
Festus Omaku <fomaku3@gmail.com>
2023-02-07 18:30:00 +0100

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" creating a class that inherits from basegeometry """
+
+
+class Rectangle(BaseGeometry):
+    """ initialize the parameters """
+
+    def __init__(self, width, height):
+        """ initializing private and public """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
