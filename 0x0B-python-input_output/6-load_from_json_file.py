c873a73a3b7dd0241d30e7b02abc133afc106530
Festus Omaku <fomaku3@gmail.com>
2023-02-08 17:44:55 +0100

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" this module creates a file from a json rep"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """ open the  file in read mode"""
+
+    with open(filename) as my_file:
+        """ load the file with json rep """
+
+        return json.loads(my_file)
