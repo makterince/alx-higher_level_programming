de9fc264a57536fcf957a9c3cdcd132de285afb6
Festus Omaku <fomaku3@gmail.com>
2023-02-08 17:34:33 +0100

    task 4

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+""" module writes an object to a text file using json """
 
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ open file with write mode """
+
+    with open(filename, 'w') as my_file:
+        """ parse through json representation"""
+
+        json.dump(my_obj, my_file)
