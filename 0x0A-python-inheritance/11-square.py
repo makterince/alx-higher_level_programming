f41ee17047ceb521cf18524fef96acfef3f5ba93
Festus Omaku <fomaku3@gmail.com>
2023-02-08 01:00:58 +0100

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" creating a class that inherits from basegeometry """
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ inherits from the class of rectangle """
+
+    def __init__(self, size):
+        """ initialize the size """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ returns area of square """
+
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """ printing format """
+
+        return ("[Square] {}/{}".format(self.__width, self.__height))
