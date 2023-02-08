23da14eca0ba2e12f7f313af444b59661652762b
Festus Omaku <fomaku3@gmail.com>
2023-02-08 18:20:13 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,13 +6,15 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+
 
 def add_item(*args):
     """ lists all arguments """
 
-    items = load_from_json_file("add_item.json")
+    items = load_from_json_file(filename)
     items += list(args)
-    save_to_json_file(items, "add_item.json")
+    save_to_json_file(items, filename)
 
 
 if __name__ == '__main__':
