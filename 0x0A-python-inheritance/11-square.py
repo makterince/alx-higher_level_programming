3e6e265a563ba5838859c9382613bfd9d285c145
Festus Omaku <fomaku3@gmail.com>
2023-02-08 01:03:04 +0100

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -21,4 +21,4 @@ class Square(Rectangle):
     def __str__(self):
         """ printing format """
 
-        return ("[Square] {}/{}".format(self.__width, self.__height))
+        return ("[Square] {}/{}".format(self.__size, self.__size))
