17fc70bdb5b071f8abbb84a629e83946ccc986a8
Festus Omaku <fomaku3@gmail.com>
2023-02-09 00:49:48 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    This module contains a Student class
+"""
+
+
+class Student:
+    """ A student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes self """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves the dictionary representation of the instance """
+
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) != str:
+                    return self.__dict__
+            return {key: self.__dict__[key] for key in attrs if key in
+                    self.__dict__}
+
+        return self.__dict__
