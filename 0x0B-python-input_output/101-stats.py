852b50983e3e93655e34e40819160790e8118efa
Festus Omaku <fomaku3@gmail.com>
2023-02-09 01:38:20 +0100

    err

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -33,24 +33,24 @@ class Magic:
             if self.dic[key] != 0:
                 print("{}: {:d}".format(key, self.dic[key]))
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
         magic = Magic()
         magic.init_dic()
         nlines = 0
 
-        try:
-            for line in sys.stdin:
-                if nlines % 10 == 0 and nlines != 0:
-                    magic.print_info()
+    try:
+        for line in sys.stdin:
+            if nlines % 10 == 0 and nlines != 0:
+                magic.print_info()
 
-                try:
-                    list_line = [x for x in line.split(" ") if x.strip()]
-                    magic.add_status_code(list_line[-2])
-                    magic.size += int(list_line[-1].strip("\n"))
-                except Exception:
-                    pass
-                nlines += 1
-        except KeyboardInterrupt:
-            magic.print_info()
-            raise
+            try:
+                list_line = [x for x in line.split(" ") if x.strip()]
+                magic.add_status_code(list_line[-2])
+                magic.size += int(list_line[-1].strip("\n"))
+            except Exception:
+                pass
+            nlines += 1
+    except KeyboardInterrupt:
         magic.print_info()
+        raise
+    magic.print_info()
