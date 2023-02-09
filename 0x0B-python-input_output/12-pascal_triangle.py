4736bf4daf3da59258b812d8185d697b76ede035
Festus Omaku <fomaku3@gmail.com>
2023-02-09 01:18:52 +0100

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,23 +11,23 @@ def pascal_triangle(n):
     tri_list = []
     if n <= 0:
         return tri_list
-    
+
     tri_list.append([1])
     if n == 1:
         return tri_list
-    
+
     tri_list.append([1, 1])
     if n == 2:
         return tri_list
-    
+
     for i in range(2, n):
         row = [1]
         prev_row = tri_list[i - 1]
-        
+
         for j in range(len(prev_row) - 1):
             row.append(prev_row[j] + prev_row[j + 1])
-            
+
         row.append(1)
         tri_list.append(row)
-        
+
     return tri_list
