d679ab808cadaa7aa8658f6052f7e4ce9b1b3b3d
Festus Omaku <fomaku3@gmail.com>
2023-04-11 23:38:16 +0100

    xtimes

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 function Xtimes(x, theFunction) {
-	for (let i = 0; i < x; i++) {
-		return (theFunction());
+	if (x <= 0 ) {
+		return ;
 	}
-};
+	theFunction();
+	Xtimes(x - 1, theFunction);
+}
 module.exports.Xtimes = Xtimes;
