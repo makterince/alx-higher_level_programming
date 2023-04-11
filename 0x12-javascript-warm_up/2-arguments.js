59024a6f134e15ea582bdc36d6618d61d9a97b0e
Festus Omaku <fomaku3@gmail.com>
2023-04-12 00:23:43 +0100

    fixing errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const argumen = process.argv.slice (2);
+const argumen = process.argv.slice(2);
 if (argumen.length === 0) {
-	console.log('No argument');
-}
-else if (argumen.length === 1) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+  console.log('No argument');
+} else if (argumen.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
