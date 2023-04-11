eec7b77657eac088f36d1d5bd212f5c89f6b8483
Festus Omaku <fomaku3@gmail.com>
2023-04-12 00:11:51 +0100

    incr function

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
 	value: 12
 };
 console.log(myObject);
-function incr(value) {
-	value++;
-}
+myObject.incr = function() {
+	this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
