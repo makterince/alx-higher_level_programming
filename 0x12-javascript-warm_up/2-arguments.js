0806c211c74a7abfb888ac4a1abe874512d45bea
Festus Omaku <fomaku3@gmail.com>
2023-04-11 15:02:52 +0100

    checking

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 function argumentChecker() {
 	if (arguments.length === 0) {
-		console.log("No argument");
+		console.log('No argument');
 	}
 	else if (arguments.length === 1) {
-		console.log("Argument found");
+		console.log('Argument found');
 	}
 	else {
-		console.log("Arguments found");
+		console.log('Arguments found');
 	}
 }
+argumentChecker();
+argumentChecker('Best');
+argumentChecker('Best', 'School');
