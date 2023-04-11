00772b785fe6c1df9e4c406af9af8b5aca6f1045
Festus Omaku <fomaku3@gmail.com>
2023-04-11 18:10:14 +0100

    multiple line

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let multiLine = [];
+multiLine[0] = 'C is fun';
+multiLine[1] = 'Python is cool';
+multiLine[2] = 'Javascript is amazing';
+for (i = 0; i < multiLine.length; i++) {
+	console.log(multiLine[i]);
+}
