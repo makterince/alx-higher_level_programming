43ee3019316cde29b022b160096d60e9479783b5
Festus Omaku <fomaku3@gmail.com>
2023-04-12 23:27:46 +0100

    empty class rectangle

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
-	width;
-	height;
-
-	constructor(width, height) {
-		this.width = width;
-		this.height = height;
-	}
-	getArea() {
-		return ((this.width) * (this.height));
-	}
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
 }
