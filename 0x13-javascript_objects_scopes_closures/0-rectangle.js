c9d70fd560f977cbe6f0fa529ec6df18535c1202
Festus Omaku <fomaku3@gmail.com>
2023-04-13 21:42:53 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const Rectangle = class {
-  constructor (width, height) {
-    this.width = width;
-    this.height = height;
-  }
+module.exports.Rectangle = class Rectangle {
+  //empty
 };
