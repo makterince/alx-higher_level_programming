790078b5d11288e2fbce1a1773c29abbc1f3b79d
Festus Omaku <fomaku3@gmail.com>
2023-04-13 22:28:37 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor(w, h) {
-		if (w <= 0 || h <= 0 || !Number.isInteger(w)
-			|| !Number.isInteger(h)) {
-			return {};
-		}
-		this.width = w;
-		this.height = h;
-	}
-	
-	print() {
-		let rectangle = '';
-		for (let i = 0; i < this.height; i++) {
-			for (let j = 0; j < this.width; j++) {
-				rectangle += 'X ';
-			}
-			rectangle += '\n';
-		}
-		console.log(rectangle);
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rectangle = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rectangle += 'X ';
+      }
+      rectangle += '\n';
+    }
+    console.log(rectangle);
+  }
 }
 exports.Rectangle = Rectangle;
