f0ef41b29fa223f348ee3ae659912baf6e961354
Festus Omaku <fomaku3@gmail.com>
2023-04-17 22:58:20 +0100

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""  lists all states from database """
+""" gets user input"""
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    curs = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+state_name = sys.argv[4]
 
-    cur.close()
+db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                     passwd=mysql_password, db=database_name)
+
+cursor = db.cursor()
+query = "SELECT * FROM states WHERE name = '{}'
+ORDER BY id ASC".format(state_name)
+cursor.execute(query)
+rows = cursor.fetchall()
+if rows:
+    for row in rows:
+        print("({}, '{}')".format(row[0], row[1]))
+    else:
+        print("No results found.")
+
+    cursor.close()
     db.close()
