fe8a4d3618b566ac5bb4fbc1afa9705fc4ada0b6
Festus Omaku <fomaku3@gmail.com>
2023-04-17 09:04:06 +0100

    statesj

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,15 +8,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    
     db = MySQLdb.connect(host="localhost",
             port=3306,
-            user=username,
-            passwd=password,
-            db=db_name)
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
     
     cursor = db.cursor()
     
