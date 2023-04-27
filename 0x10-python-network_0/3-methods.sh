33225b55a00cc3a129c4bcee262200d4710e96ec
makterince <fomaku3@gmail.com>
2023-04-27 11:43:19 -0700

    trying

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send an OPTIONS request to the URL and display the allowed methods
-curl -s -X OPTIONS -i $1 | grep "Allow:" | tr -d '\r\n' | sed 's/Allow: //'
+curl -sIX OPTIONS "$1" | grep -E "Allow: " | cut -d " " -f 2-
