1b99cece002ba5259653617d940c421e1d83a2bc
Festus Omaku <fomaku3@gmail.com>
2023-04-28 21:25:18 +0100

    post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     from sys import argv
     from urllib.request import urlopen
     from urllib.parse import urlencode
-    
+
     data = urlencode({"email": argv[2]}).encode("ascii")
     with urlopen(argv[1], data) as r:
         print(r.read().decode("utf-8"))
-
