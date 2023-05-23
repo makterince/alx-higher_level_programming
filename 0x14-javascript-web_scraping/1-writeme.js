272b23f99b4e74d82aba23f4ed8d9c4413aa44bc
makterince <fomaku3@gmail.com>
2023-05-23 12:06:57 -0700

    enjoying this

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+	console.error('File path and string not included');
+	process.exit(1);
+}
+
+const content = process.argv[3];
+const filePath = process.argv[2]
+fs.writeFile(filePath, content, 'utf8', (error) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+	console.log('succesful');
+});
