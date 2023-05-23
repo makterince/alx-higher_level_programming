e0d692b1208a9dea97e07f859bd9e59a47df03ac
makterince <fomaku3@gmail.com>
2023-05-23 11:23:57 -0700

    correction

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
-fs.readFile('path/to/file.txt', 'utf8', (error, data) => {
+const fs = require('fs');
+if (process.argv.length < 3) {
+	console.error(error);
+	process.exit(1);
+}
+
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf8', (error, data) => {
 	if (error) {
 		console.error(error);
 		return;
