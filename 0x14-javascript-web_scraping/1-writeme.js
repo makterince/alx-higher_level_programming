890194c0940dfca9757a8d4282d96a1c75555e69
makterince <fomaku3@gmail.com>
2023-05-23 12:08:40 -0700

    enjoying this

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,16 +2,16 @@
 const fs = require('fs');
 
 if (process.argv.length < 4) {
-	console.error('File path and string not included');
-	process.exit(1);
+  console.error('File path and string not included');
+  process.exit(1);
 }
 
 const content = process.argv[3];
-const filePath = process.argv[2]
+const filePath = process.argv[2];
 fs.writeFile(filePath, content, 'utf8', (error) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
-	console.log('succesful');
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('succesful');
 });
